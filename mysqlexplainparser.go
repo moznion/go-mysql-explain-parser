5fc615b86a4049c793fd6598192839c40651db10
moznion <moznion@gmail.com>
2014-06-25 21:24:55 +0900

    Add function to parse vertical explain

--- mysqlexplainparser.go
+++ mysqlexplainparser.go
@@ -6,30 +6,53 @@ import (
 	"github.com/moznion/go-text-visual-width"
 )
 
-// Parse returns the result of parsed EXPLAIN as array of map
-func Parse(explain string) []map[string]string {
-	var rows = regexp.MustCompile("\r?\n").Split(explain, -1)
+type stringArray []string
+
+func (rows *stringArray) removeUnnecessaryLinesOnTop() {
+	_rows := *rows
 
 	// Skip the blank line(s) that are on the top
-	for _, row := range rows {
+	for _, row := range _rows {
 		if row != "" {
 			break
 		}
-		rows = rows[1:]
+		_rows = _rows[1:]
 	}
-	rows = rows[1:] // Skip the top of outline
+	*rows = _rows[1:] // Skip the top of outline
+}
+
+func (rows *stringArray) removeUnnecessaryLinesOnBottom() {
+	_rows := *rows
+
+	regexToRemoveQueryResult := regexp.MustCompile("^[0-9]+[ \t]+rows") // e.g. "2 rows in set, 1 warning (0.00 sec)"
+	regexToDetectLineOfTable := regexp.MustCompile("^\\+-+")
 
 	// Skip bottom unnecessary line(s)
 	for {
-		lastPos := len(rows) - 1
-		lastLine := rows[lastPos]
-		rows = rows[:lastPos]
+		lastPos := len(_rows) - 1
+		lastLine := _rows[lastPos]
+		_rows = _rows[:lastPos]
 
-		regexToRemoveQueryResult := regexp.MustCompile("^[0-9]+[ \t]+rows") // e.g. "2 rows in set, 1 warning (0.00 sec)"
-		if (!regexToRemoveQueryResult.MatchString(lastLine) && lastLine != "") || rows == nil {
+		if (!regexToRemoveQueryResult.MatchString(lastLine) && lastLine != "") || _rows == nil {
+			if !regexToDetectLineOfTable.MatchString(lastLine) {
+				_rows = _rows[:lastPos+1]
+			}
 			break
 		}
 	}
+
+	*rows = _rows
+}
+
+func (rows *stringArray) removeUnnecessaryLinesOnBothEnds() {
+	rows.removeUnnecessaryLinesOnTop()
+	rows.removeUnnecessaryLinesOnBottom()
+}
+
+// Parse returns the result of parsed EXPLAIN as array of map
+func Parse(explain string) []map[string]string {
+	rows := stringArray(regexp.MustCompile("\r?\n").Split(explain, -1))
+	rows.removeUnnecessaryLinesOnBothEnds()
 
 	var indexRow string
 	indexRow, rows = rows[0], rows[2:]
@@ -52,7 +75,7 @@ func Parse(explain string) []map[string]string {
 
 	parsed := make([]map[string]string, len(rows))
 	for i, row := range rows {
-		var parsedRow = make(map[string]string)
+		parsedRow := make(map[string]string)
 		for j, index := range indexes {
 			var column string
 			row = row[1:]
@@ -65,6 +88,31 @@ func Parse(explain string) []map[string]string {
 		}
 		parsed[i] = parsedRow
 	}
+
+	return parsed
+}
+
+// ParseVertical returns the result of parsed EXPLAIN as vertical as array of map
+func ParseVertical(explain string) []map[string]string {
+	rows := stringArray(regexp.MustCompile("\r?\n").Split(explain, -1))
+	rows.removeUnnecessaryLinesOnBothEnds()
+
+	var parsed []map[string]string
+	parsedRow := make(map[string]string)
+	regexToDetectRowSeparator := regexp.MustCompile("^\\*+")
+	regexToGetKeyValue := regexp.MustCompile("^[ \t]*(.+)?:([ \t]+(.+))?")
+
+	for _, row := range rows {
+		if regexToDetectRowSeparator.MatchString(row) {
+			parsed = append(parsed, parsedRow)
+			parsedRow = make(map[string]string)
+			continue
+		}
+		matched := regexToGetKeyValue.FindStringSubmatch(row)
+		key, value := matched[1], matched[3]
+		parsedRow[key] = value
+	}
+	parsed = append(parsed, parsedRow)
 
 	return parsed
 }
--- mysqlexplainparser_test.go
+++ mysqlexplainparser_test.go
@@ -46,19 +46,19 @@ func expected() []map[string]string {
 	}
 }
 
-func TestBasicCase(t *testing.T) {
+func TestParseBasicCase(t *testing.T) {
 	RegisterTestingT(t)
 
 	Expect(Parse(explain())).To(Equal(expected()))
 }
 
-func TestNoisyLinesAreOnBothEnds(t *testing.T) {
+func TestParseNoisyLinesAreOnBothEnds(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(Parse("\n\n" + explain() + "2 rows in set, 1 warning (0.00 sec)\n\n")).To(Equal(expected()))
+	Expect(Parse("\n\n" + explain() + "\n2 rows in set, 1 warning (0.00 sec)\n\n")).To(Equal(expected()))
 }
 
-func TestMultiByteCharacter(t *testing.T) {
+func TestParseMultiByteCharacter(t *testing.T) {
 	RegisterTestingT(t)
 
 	explain := `+----+-------------+-----------+-------+---------------+--------+---------+------+------+-------------+
@@ -83,4 +83,76 @@ func TestMultiByteCharacter(t *testing.T) {
 	}
 
 	Expect(Parse(explain)).To(Equal(expected))
+}
+
+func explainVertical() string {
+	return `*************************** 1. row ***************************
+           id: 1
+  select_type: PRIMARY
+        table: t1
+         type: index
+possible_keys: NULL
+          key: PRIMARY
+      key_len: 4
+          ref: NULL
+         rows: 4
+     filtered: 100.00
+        Extra:
+*************************** 2. row ***************************
+           id: 2
+  select_type: SUBQUERY
+        table: t2
+         type: index
+possible_keys: a
+          key: a
+      key_len: 5
+          ref: NULL
+         rows: 3
+     filtered: 100.00
+        Extra: Using index`
+}
+
+func TestParseVerticalBasicCase(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(ParseVertical(explainVertical())).To(Equal(expected()))
+}
+
+func TestParseVerticalNoisyLinesAreOnBothEnds(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(ParseVertical("\n\n" + explainVertical() + "\n2 rows in set, 1 warning (0.00 sec)\n\n")).To(Equal(expected()))
+}
+
+func TestParseVerticalMultiByteCharacter(t *testing.T) {
+	RegisterTestingT(t)
+
+	explainVertical := `*************************** 1. row ***************************
+           id: 1
+  select_type: SIMPLE
+        table: てすつ
+         type: index
+possible_keys: NULL
+          key: ほげ
+      key_len: 4
+          ref: NULL
+         rows: 5
+        Extra: Using index`
+
+	expected := []map[string]string{
+		{
+			"id":            "1",
+			"select_type":   "SIMPLE",
+			"table":         "てすつ",
+			"type":          "index",
+			"possible_keys": "NULL",
+			"key":           "ほげ",
+			"key_len":       "4",
+			"ref":           "NULL",
+			"rows":          "5",
+			"Extra":         "Using index",
+		},
+	}
+
+	Expect(ParseVertical(explainVertical)).To(Equal(expected))
 }
